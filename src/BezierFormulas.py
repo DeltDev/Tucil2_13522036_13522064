452c9e953e1763ea9ec7154f5fbca0ad93f51232
DeltDev <deltenc88@gmail.com>
2024-03-12 14:32:30 +0700

    Added Quadratic Bezier Formula

--- src/BezierFormulas.py
+++ src/BezierFormulas.py
@@ -3,3 +3,8 @@ def LinearBezier(ControlPoint0: Point, ControlPoint1 : Point, t: int) -> Point: 
     Pt = Point((ControlPoint0.x + t * (ControlPoint1.x - ControlPoint0.x)), (ControlPoint0.y + t * (ControlPoint1.y - ControlPoint0.y)))
     return Pt
 
+def QuadraticBezier(ControlPoint0 : Point, ControlPoint1: Point, ControlPoint2: Point, t: int) -> Point: #Persamaan kurva Bezier Kuadratik
+    Q1 = LinearBezier(ControlPoint0, ControlPoint1, t)
+    Q2 = LinearBezier(ControlPoint1, ControlPoint2, t)
+    R = LinearBezier(Q1,Q2,t)
+    return R
