dd1c73546aa2bebae8163564c46b70e367ad2ff5
DeltDev <deltenc88@gmail.com>
2024-03-13 10:55:31 +0700

    GUI Window loop finished

--- src/main.py
+++ src/main.py
@@ -1,26 +1,35 @@
 #import GUI dan visualisasi kurva bezier
 import tkinter as tk
 from tkinter import ttk
-#Setup window GUI
-window = tk.Tk()
-window.configure(bg="gray")
-window.geometry("540x720")
-window.resizable(False,False)
-window.title("Simulasi Kurva Bezier")
+#Setup window root GUI 
+root = tk.Tk()
+root.configure(bg="gray")
+root.geometry("540x720")
+root.resizable(False,False)
+root.title("Simulasi Kurva Bezier")
 
-#frame input
-input_frame = ttk.Frame(window)
+input_frame = ttk.Frame(root)
 input_frame.pack(padx=10,pady=10,fill="x",expand=True)
 
-#komponen GUI
-testLabel1 = ttk.Label(input_frame,text="Test 1:")
-testLabel1.pack(padx=10,fill="x",expand=True)
-TEST_ENTRY1 = tk.StringVar()
-testEntry1 = ttk.Entry(input_frame,textvariable=TEST_ENTRY1)
-testEntry1.pack(padx=10,fill="x",expand=True)
-#perintah tombol
-def buttonClick():
-    print(TEST_ENTRY1.get())
-testButton1 = ttk.Button(input_frame,text="tampilkan teks",command=buttonClick)
-testButton1.pack(fill='x',expand=True,padx=10,pady=10)
-window.mainloop()
+def ReopenRootWindow(newWindow):
+    newWindow.destroy()
+    root.deiconify()
+def OpenNextWindow():
+    newWindow = tk.Toplevel(root)
+
+    newWindow.title("Window baru")
+
+    newWindow.geometry("200x200")
+    newWindowFrame = ttk.Frame(newWindow)
+    newWindowFrame.pack(padx=10,pady=10,fill="x",expand=True)
+    newWindowLabel = ttk.Label(newWindow,text = "Ini window baru")
+    newWindowLabel.pack(padx=10,pady=10,fill="x",expand=True)
+    newWindowReturn = ttk.Button(newWindow,text="Kembali ke root",command=lambda: ReopenRootWindow(newWindow))
+    newWindowReturn.pack(padx=10,pady=10,fill="x",expand=True)
+    root.withdraw()
+    
+NextWindowButton = ttk.Button(input_frame,text="Ke Window Berikutnya",command=OpenNextWindow)
+NextWindowButton.pack(padx=10,pady=10,fill="x",expand=True)
+
+
+root.mainloop()
