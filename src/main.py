85097a8eb23b17eedf54650482e5a1c6c9d6abe0
DeltDev <deltenc88@gmail.com>
2024-03-13 09:52:23 +0700

    GUI WIP 2

--- src/main.py
+++ src/main.py
@@ -10,5 +10,17 @@ window.title("Simulasi Kurva Bezier")
 
 #frame input
 input_frame = ttk.Frame(window)
+input_frame.pack(padx=10,pady=10,fill="x",expand=True)
 
+#komponen GUI
+testLabel1 = ttk.Label(input_frame,text="Test 1:")
+testLabel1.pack(padx=10,fill="x",expand=True)
+TEST_ENTRY1 = tk.StringVar()
+testEntry1 = ttk.Entry(input_frame,textvariable=TEST_ENTRY1)
+testEntry1.pack(padx=10,fill="x",expand=True)
+#perintah tombol
+def buttonClick():
+    print(TEST_ENTRY1.get())
+testButton1 = ttk.Button(input_frame,text="tampilkan teks",command=buttonClick)
+testButton1.pack(fill='x',expand=True,padx=10,pady=10)
 window.mainloop()
